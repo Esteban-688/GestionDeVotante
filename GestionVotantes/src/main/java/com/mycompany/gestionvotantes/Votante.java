98989af9b1957b2e27a97542983952e7422f6279
edomarava <56361998+edomarava@users.noreply.github.com>
2023-04-25 17:17:01 -0400

    funcion cargar votante
    se lee el csv y se guardan sus atributos

--- GestionVotantes/src/main/java/com/mycompany/gestionvotantes/Votante.java
+++ GestionVotantes/src/main/java/com/mycompany/gestionvotantes/Votante.java
@@ -97,4 +97,23 @@ public class Votante {
     public void setNMesa(int nMesa) {
         this.nMesa = nMesa;
     }
+
+     //Funcion cargar datos desde un csv
+     public static void cargarInscritos(ArrayList<Votante> mesaVotante) throws IOException{
+		String filePath = "personasInscritas.txt";
+        BufferedReader lectorTxt = new BufferedReader( new FileReader( filePath ) );
+		String lineText = null;
+		
+		while ((lineText = lectorTxt.readLine()) != null) {
+			Votante bi = new Votante();
+			String[] arr = lineText.split(",");
+			bi.setNMesa(Integer.parseInt(arr[0]));
+			bi.setName(arr[1]);
+			bi.setRut(arr[2]);
+			mesaVotante.add(bi);
+		}
+		
+		lectorTxt.close();
+	}
+	
 }
